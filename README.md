eff5642f2142d1bfa3c682131ff364804f649c3d
Robin Cornett <hello@robincornett.com>
2016-02-13 09:46:45 -0500

    Update readme and helpful text

--- README.md
+++ README.md
@@ -23,47 +23,35 @@ Then go to your Plugins screen and click __Activate__.
 
 ## Frequently Asked Questions
 
-### How do I change the twitter handle (@username)?
+### What if I want to change...
 
-Use a filter:
+As of 1.0.0, Scriptless Social Sharing now includes a settings page, so although the original filters still exist, you can change a lot of things quite easily without touching any code. Go to Settings > Scriptless Social Sharing to set buttons, twitter handle, and stylesheets.
+
+### What if I want to move where the buttons are output?
+
+The social sharing buttons are added to the end of your post content using `the_content` filter, so they'll work with any theme. If you want to move them, you can remove the original filter, and add the buttons using your own action. Example:
 
 ```php
-add_filter( 'scriptlesssocialsharing_twitter_handle', 'rgc_twitter_handle' );
-function rgc_twitter_handle( $handle ) {
-    return 'robincornett';
+remove_filter( 'the_content', 'scriptlesssocialsharing_print_buttons', 99 );
+add_action( 'genesis_entry_content', 'prefix_scriptlesssocialsharing_buttons_entry_content', 5 );
+function prefix_scriptlesssocialsharing_buttons_entry_content() {
+	echo wp_kses_post( scriptlesssocialsharing_do_buttons() );
 }
 ```
 
-### What if I want to change the heading?
+### I'd like to add sharing buttons to a different content type.
 
-Good question. It's another filter:
+Scriptless Social Sharing adds buttons to posts only. You can change this easily with a simple filter:
 
 ```php
-add_filter( 'scriptlesssocialsharing_heading', 'rgc_social_heading' );
-function rgc_social_heading( $heading ) {
-    return 'Be a friend: share this post.';
+add_filter( 'scriptlesssocialsharing_post_types', 'prefix_add_download_buttons' );
+function prefix_add_download_buttons( $post_types ) {
+	$post_types[] = 'download';
+	return $post_types;
 }
 ```
 
-### What if I want to remove a button?
-
-It's another filter:
-
-```php
-add_filter( 'scriptlesssocialsharing_default_buttons', 'rgc_test_remove' );
-function rgc_test_remove( $buttons ) {
-    unset( $buttons['pinterest'] );
-    return $buttons;
-}
-```
-
-### I don't want to use FontAwesome.
-
-Filter:
-
-```php
-add_filter( 'scriptlesssocialsharing_fontawesome', '__return_false' );
-```
+This example adds buttons to the download post type (Easy Digital Downloads) as well as to posts.
 
 ## Changelog
 
--- includes/class-scriptlesssocialsharing-settings.php
+++ includes/class-scriptlesssocialsharing-settings.php
@@ -201,31 +201,17 @@ class ScriptlessSocialSharingSettings {
 	 * @since 2.4.0
 	 */
 	public function general_section_description() {
-		$description = __( 'You can set the default isotope settings here.', 'scriptless-social-sharing' );
-		printf( '<p>%s</p>', wp_kses_post( $description ) );
-	}
-
-	/**
-	 *
-	 */
-	public function cpt_section_description() {
-		$description = __( 'Set the isotope settings for each post type.', 'scriptless-social-sharing' );
+		$description = __( 'Scriptless Social Sharing tries to be helpful, but you can also disable whatever you need.', 'scriptless-social-sharing' );
 		printf( '<p>%s</p>', wp_kses_post( $description ) );
 	}
 
 	/**
 	 * Generic callback to create a checkbox setting.
 	 *
-	 * @since 3.0.0
+	 * @since 1.0.0
 	 */
 	public function do_checkbox( $args ) {
-		$setting = isset( $this->setting[ $args['setting'] ] ) ? $this->setting[ $args['setting'] ] : 0;
-		if ( isset( $args['setting_name'] ) && 'buttons' === $args['setting_name'] ) {
-			$setting = $this->setting[ $args['setting_name'] ][ $args['network'] ];
-		}
-		if ( isset( $args['setting_name'] ) && 'styles' === $args['setting_name'] ) {
-			$setting = $this->setting[ $args['setting_name'] ][ $args['name'] ];
-		}
+		$setting = $this->get_checkbox_setting( $args );
 		printf( '<input type="hidden" name="%s[%s]" value="0" />', esc_attr( $this->page ), esc_attr( $args['setting'] ) );
 		printf( '<label for="%1$s[%2$s]"><input type="checkbox" name="%1$s[%2$s]" id="%1$s[%2$s]" value="1" %3$s class="code" />%4$s</label>',
 			esc_attr( $this->page ),
@@ -237,9 +223,23 @@ class ScriptlessSocialSharingSettings {
 	}
 
 	/**
+	 * Get the current value for the checkbox.
+	 * @param $args
+	 *
+	 * @return int
+	 */
+	protected function get_checkbox_setting( $args ) {
+		$setting = isset( $this->setting[ $args['setting'] ] ) ? $this->setting[ $args['setting'] ] : 0;
+		if ( isset( $args['setting_name'] ) ) {
+			$setting = $this->setting[ $args['setting_name'] ][ $args['name'] ];
+		}
+		return $setting;
+	}
+
+	/**
 	 * Generic callback to create a number field setting.
 	 *
-	 * @since 3.0.0
+	 * @since 1.0.0
 	 */
 	public function do_number( $args ) {
 		$setting = isset( $this->setting[$args['setting']] ) ? $this->setting[$args['setting']] : 0;
@@ -261,7 +261,7 @@ class ScriptlessSocialSharingSettings {
 	/**
 	 * Generic callback to create a select/dropdown setting.
 	 *
-	 * @since 3.0.0
+	 * @since 1.0.0
 	 */
 	public function do_select( $args ) {
 		$function = 'pick_' . $args['options'];
@@ -279,7 +279,7 @@ class ScriptlessSocialSharingSettings {
 	/**
 	 * Generic callback to create a text field.
 	 *
-	 * @since 3.0.0
+	 * @since 1.0.0
 	 */
 	public function do_text_field( $args ) {
 		printf( '<input type="text" id="%3$s[%1$s]" name="%3$s[%1$s]" value="%2$s" class="regular-text" />', esc_attr( $args['setting'] ), esc_attr( $this->setting[$args['setting']] ), esc_attr( $this->page ) );
@@ -302,6 +302,11 @@ class ScriptlessSocialSharingSettings {
 		printf( '<p class="description">%s</p>', wp_kses_post( $description ) );
 	}
 
+	/**
+	 * Add checkboxes to enable/disable each style setting.
+	 *
+	 * @param $args
+	 */
 	public function do_styles( $args ) {
 		$styles = $this->get_styles();
 		foreach ( $styles as $style ) {
@@ -317,6 +322,10 @@ class ScriptlessSocialSharingSettings {
 
 	}
 
+	/**
+	 * Define the array of style settings.
+	 * @return array
+	 */
 	protected function get_styles() {
 		$styles = array(
 			array(
@@ -345,7 +354,7 @@ class ScriptlessSocialSharingSettings {
 				'setting'      => "{$args['setting']}][{$network['name']}",
 				'label'        => $network['label'],
 				'setting_name' => $args['setting'],
-				'network'      => $network['name'],
+				'name'         => $network['name'],
 			);
 			$this->do_checkbox( $network_args );
 			echo '<br />';
@@ -354,6 +363,8 @@ class ScriptlessSocialSharingSettings {
 	}
 
 	/**
+	 * Build the array of networks for choices.
+	 * Using the filter to add/remove networks will change the settings page and the output.
 	 * @return mixed|void
 	 */
 	public function get_networks() {
@@ -387,6 +398,7 @@ class ScriptlessSocialSharingSettings {
 	}
 
 	/**
+	 * Description for the heading.
 	 * @return string|void
 	 */
 	protected function heading_description() {
@@ -394,6 +406,7 @@ class ScriptlessSocialSharingSettings {
 	}
 
 	/**
+	 * Description for the email subject setting.
 	 * @return string|void
 	 */
 	protected function email_subject_description() {
@@ -407,7 +420,7 @@ class ScriptlessSocialSharingSettings {
 	 *
 	 * @return array            validated values
 	 *
-	 * @since 3.0.0
+	 * @since 1.0.0
 	 */
 	public function do_validation_things( $new_value ) {
 
