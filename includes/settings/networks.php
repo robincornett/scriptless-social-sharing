f89e4cfdd1410341efad40562baff7c8016bc92a
Robin Cornett <hello@robincornett.com>
2018-12-01 12:54:19 -0500

    Add default order parameter to networks
    If the order filter posted on Github is used, this is needed to prevent
    PHP errors. Not used for anything else. Adding because that's been out
    there for so long.

--- includes/settings/networks.php
+++ includes/settings/networks.php
@@ -9,34 +9,42 @@ return apply_filters(
 		'twitter'   => array(
 			'name'  => 'twitter',
 			'label' => __( 'Twitter', 'scriptless-social-sharing' ),
+			'order' => 0,
 		),
 		'facebook'  => array(
 			'name'  => 'facebook',
 			'label' => __( 'Facebook', 'scriptless-social-sharing' ),
+			'order' => 0,
 		),
 		'google'    => array(
 			'name'  => 'google',
 			'label' => __( 'Google+', 'scriptless-social-sharing' ),
+			'order' => 0,
 		),
 		'pinterest' => array(
 			'name'  => 'pinterest',
 			'label' => __( 'Pinterest', 'scriptless-social-sharing' ),
+			'order' => 0,
 		),
 		'linkedin'  => array(
 			'name'  => 'linkedin',
 			'label' => __( 'Linkedin', 'scriptless-social-sharing' ),
+			'order' => 0,
 		),
 		'email'     => array(
 			'name'  => 'email',
 			'label' => __( 'Email', 'scriptless-social-sharing' ),
+			'order' => 0,
 		),
 		'reddit'    => array(
 			'name'  => 'reddit',
 			'label' => __( 'Reddit', 'scriptless-social-sharing' ),
+			'order' => 0,
 		),
 		'whatsapp'  => array(
 			'name'  => 'whatsapp',
 			'label' => __( 'WhatsApp', 'scriptless-social-sharing' ),
+			'order' => 0,
 		),
 	)
 );
