a467ac6661be2420d6967ac89325a47ede55480d
Robin Cornett <hello@robincornett.com>
2019-04-24 21:23:19 -0400

    Refactor shortcode class

--- includes/output/class-scriptlesssocialsharing-output-shortcode.php
+++ includes/output/class-scriptlesssocialsharing-output-shortcode.php
@@ -14,8 +14,40 @@ class ScriptlessSocialSharingOutputShortcode extends ScriptlessSocialSharingOutp
 	 * @return string
 	 */
 	public function shortcode( $atts ) {
-		$setting  = $this->get_setting();
-		$defaults = array(
+		$atts    = $this->update_attributes( $atts );
+		$output  = $atts['before'];
+		$output .= $this->heading( $atts['heading'] );
+		$output .= $atts['inner_before'];
+		$output .= $this->add_shortcode_buttons( $atts['buttons'] );
+		$output .= $atts['inner_after'];
+		$output .= $atts['after'];
+
+		return $output;
+	}
+
+	/**
+	 * Update the shortcode attributes and merge with defaults.
+	 * @since 3.0.0
+	 *
+	 * @param $atts
+	 * @return array
+	 */
+	private function update_attributes( $atts ) {
+		$defaults = $this->get_defaults();
+
+		return shortcode_atts( $defaults, $atts, 'scriptless' );
+	}
+
+	/**
+	 * Define the shortcode default attributes.
+	 * @since 3.0.0
+	 *
+	 * @return array
+	 */
+	private function get_defaults() {
+		$setting = $this->get_setting();
+
+		return array(
 			'before'       => '<div class="scriptlesssocialsharing">',
 			'after'        => '</div>',
 			'inner_before' => '<div class="' . $this->get_button_container_class( $setting ) . '">',
@@ -23,19 +55,25 @@ class ScriptlessSocialSharingOutputShortcode extends ScriptlessSocialSharingOutp
 			'heading'      => $setting['heading'],
 			'buttons'      => '',
 		);
-		$atts     = shortcode_atts( $defaults, $atts, 'scriptless' );
-		$buttons  = $this->get_all_buttons();
-		$passed   = $atts['buttons'] ? explode( ',', $atts['buttons'] ) : array();
-		$output   = $atts['before'];
-		$output  .= $this->heading( $atts['heading'] );
-		$output  .= $atts['inner_before'];
-		foreach ( $buttons as $button ) {
+	}
+
+	/**
+	 * Get the buttons to add to the shortcode output.
+	 * @since 3.0.0
+	 *
+	 * @param $buttons
+	 * @return string
+	 *
+	 */
+	private function add_shortcode_buttons( $buttons ) {
+		$all_buttons = $this->get_all_buttons();
+		$passed      = $buttons ? explode( ',', $buttons ) : array();
+		$output      = '';
+		foreach ( $all_buttons as $button ) {
 			if ( empty( $passed ) || in_array( $button['name'], $passed, true ) ) {
 				$output .= $this->build_link_markup( $button );
 			}
 		}
-		$output .= $atts['inner_after'];
-		$output .= $atts['after'];
 
 		return $output;
 	}
